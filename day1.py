9e34f132270cfcdf6f8070591636760e81125889
Sibi <sibirajkts@gmail.com>
2026-07-03 00:26:47 -0400

    week01 day1: dynamic typing and reference semantics

---
+++ day1.py
@@ -0,0 +1,20 @@
+name = "FastKVS"
+version = 1
+is_running = True
+
+print(name)
+print(version)
+print(is_running)
+print(type(name))
+print(type(version))
+print(type(is_running))
+
+name = 42
+print(name)
+print(type(name))
+
+a = [1, 2, 3]
+b = a
+b.append(4)
+print(a)
+print(b)
